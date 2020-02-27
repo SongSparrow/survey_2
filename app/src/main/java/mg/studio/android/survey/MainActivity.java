11b52ce4f081c0b6efeb7eb2ebc88ba3bedc906d
YuanZouTaXiang <qc_1e0b94c8f8d245e3a2cf612ba73eb1b8@git.code.tencent.com>
2020-02-27 23:33:28 +0800

    add report

--- app/src/main/java/mg/studio/android/survey/MainActivity.java
+++ app/src/main/java/mg/studio/android/survey/MainActivity.java
@@ -2,14 +2,21 @@ package mg.studio.android.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private CheckBox mCbAccept;
+    private String[] answers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +25,27 @@ public class MainActivity extends AppCompatActivity {
         mCbAccept = (CheckBox)findViewById(R.id.cb_accept);
     }
 
+    private String getAnswer(int rgId){
+        RadioGroup rg = (RadioGroup) findViewById(rgId);
+        int checkedId = rg.getCheckedRadioButtonId();
+        if(checkedId == -1){
+            Toast.makeText(this,
+                    R.string.empty_answer,
+                    Toast.LENGTH_SHORT).show();
+            return "";
+        }
+        RadioButton rb = (RadioButton) findViewById(checkedId);
+        return "Your answer is "+rb.getText().toString();
+    }
+
     public void onClickNext(View view){
         int next;
         if(view != null) next  = view.getId();
         else return;
         switch (next){
-            case R.id.btn_go:
+            case R.id.btn_go:{
                 if(mCbAccept.isChecked()){
+                    answers = new String[12];
                     setContentView(R.layout.question_one);
                 }else{
                     Toast.makeText(
@@ -32,48 +53,135 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case R.id.btn_next1:
+            }
+            case R.id.btn_next1:{
+                answers[0] = getAnswer(R.id.rg_question1);
+                if (answers[0].length() == 0) break;
                 setContentView(R.layout.question_two);
                 break;
-            case R.id.btn_next2:
+            }
+            case R.id.btn_next2:{
+                answers[1] = getAnswer(R.id.rg_question2);
+                if(answers[1].length() == 0) break;
                 setContentView(R.layout.question_three);
                 break;
-            case R.id.btn_next3:
+            }
+            case R.id.btn_next3:{
+                answers[2] = getAnswer(R.id.rg_question3);
+                if(answers[2].length() == 0) break;
                 setContentView(R.layout.question_four);
                 break;
-            case R.id.btn_next4:
+            }
+            case R.id.btn_next4:{
+                answers[3]="Your answer is ";
+                int [] checkedIds = new int[7];
+                checkedIds[0] = R.id.cb_music;
+                checkedIds[1] = R.id.cb_photo;
+                checkedIds[2] = R.id.cb_game;
+                checkedIds[3] = R.id.cb_business;
+                checkedIds[4] = R.id.cb_gps;
+                checkedIds[5] = R.id.cb_data;
+                checkedIds[6] = R.id.cb_others;
+                for (int i=0;i<7;i++){
+                    CheckBox cb = (CheckBox) findViewById(checkedIds[i]);
+                    if(cb.isChecked()){
+                        answers[3] +=  cb.getText().toString();
+                        answers[3] += ",";
+                    }
+                }
+                if(answers[3].equals("Your answer is ")){
+                    Toast.makeText(this,
+                            R.string.empty_answer,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                }
                 setContentView(R.layout.question_five);
                 break;
-            case R.id.btn_next5:
+            }
+            case R.id.btn_next5:{
+                answers[4]="Your answer is ";
+                int [] checkedIds = new int[7];
+                checkedIds[0] = R.id.cb_music;
+                checkedIds[1] = R.id.cb_photo;
+                checkedIds[2] = R.id.cb_game;
+                checkedIds[3] = R.id.cb_business;
+                checkedIds[4] = R.id.cb_gps;
+                checkedIds[5] = R.id.cb_data;
+                checkedIds[6] = R.id.cb_others;
+                for (int i=0;i<7;i++){
+                    CheckBox cb = (CheckBox) findViewById(checkedIds[i]);
+                    if(cb.isChecked()){
+                        answers[4] +=  cb.getText().toString();
+                        answers[4] += ",";
+                    }
+                }
+                if(answers[4].equals("Your answer is ")){
+                    Toast.makeText(this,
+                            R.string.empty_answer,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                }
                 setContentView(R.layout.question_six);
                 break;
-            case R.id.btn_next6:
+            }
+            case R.id.btn_next6:{
+                EditText editText = (EditText) findViewById(R.id.edt_answer6);
+                answers[5] = "Your answer is ";
+                answers[5] += editText.getText().toString();
+                if (answers[5].length() == 0) {
+                    Toast.makeText(this,
+                            R.string.empty_answer,
+                            Toast.LENGTH_SHORT).show();
+                    break;
+                }
                 setContentView(R.layout.question_seven);
                 break;
-            case R.id.btn_next7:
+            }
+            case R.id.btn_next7:{
+                answers[6] = getAnswer(R.id.rg_question7);
+                if(answers[6].length() == 0) break;
                 setContentView(R.layout.question_eight);
                 break;
-            case R.id.btn_next8:
+            }
+            case R.id.btn_next8:{
+                answers[7] = getAnswer(R.id.rg_question8);
+                if(answers[7].length() == 0) break;
                 setContentView(R.layout.question_nine);
                 break;
-            case R.id.btn_next9:
+            }
+            case R.id.btn_next9:{
+                answers[8] = getAnswer(R.id.rg_question9);
+                if(answers[8].length() == 0) break;
                 setContentView(R.layout.question_ten);
                 break;
-            case R.id.btn_next10:
+            }
+            case R.id.btn_next10: {
+                answers[9] = getAnswer(R.id.rg_question10);
+                if (answers[9].length() == 0) break;
                 setContentView(R.layout.question_eleven);
                 break;
-            case R.id.btn_next11:
+            }
+            case R.id.btn_next11:{
+                answers[10] = getAnswer(R.id.rg_question11);
+                if(answers[10].length() == 0) break;
                 setContentView(R.layout.question_twelve);
                 break;
-            case R.id.btn_next12:
+            }
+            case R.id.btn_next12:{
+                answers[11] = getAnswer(R.id.rg_question12);
+                if(answers[11].length() == 0) break;
                 setContentView(R.layout.finish_survey);
                 break;
+            }
             default:
                     break;
         }
     }
 
-    public void Finish(View view) {
-        super.finish();
+    public void Finish(View view){
+        Intent intent = new Intent(this, ReportActivity.class);
+        intent.putExtra("answer",answers);
+        startActivity(intent);
     }
+
 }
--- app/src/main/java/mg/studio/android/survey/ReportActivity.java
+++ app/src/main/java/mg/studio/android/survey/ReportActivity.java
@@ -2,13 +2,38 @@ package mg.studio.android.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class ReportActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        Intent intent = getIntent();
+        String[] answers = intent.getStringArrayExtra("answer");
+        try {
+            ((TextView) findViewById(R.id.txt_answer1)).setText(answers[0]);
+            ((TextView) findViewById(R.id.txt_answer2)).setText(answers[1]);
+            ((TextView) findViewById(R.id.txt_answer3)).setText(answers[2]);
+            ((TextView) findViewById(R.id.txt_answer4)).setText(answers[3]);
+            ((TextView) findViewById(R.id.txt_answer5)).setText(answers[4]);
+            ((TextView) findViewById(R.id.txt_answer6)).setText(answers[5]);
+            ((TextView) findViewById(R.id.txt_answer7)).setText(answers[6]);
+            ((TextView) findViewById(R.id.txt_answer8)).setText(answers[7]);
+            ((TextView) findViewById(R.id.txt_answer9)).setText(answers[8]);
+            ((TextView) findViewById(R.id.txt_answer10)).setText(answers[9]);
+            ((TextView) findViewById(R.id.txt_answer11)).setText(answers[10]);
+            ((TextView) findViewById(R.id.txt_answer12)).setText(answers[11]);
+        }catch (NullPointerException npe){
+            npe.printStackTrace();
+        }
+    }
+
+    public void onClickExit(View view){
+        super.finish();
     }
 }
