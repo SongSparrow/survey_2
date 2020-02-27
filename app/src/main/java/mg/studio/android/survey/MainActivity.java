5da60a2074efad99ac0f349e1a6b64c112834767
YuanZouTaXiang <qc_1e0b94c8f8d245e3a2cf612ba73eb1b8@git.code.tencent.com>
2020-02-27 16:40:06 +0800

    modify ids

--- app/src/main/java/mg/studio/android/survey/MainActivity.java
+++ app/src/main/java/mg/studio/android/survey/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
-        mCbAccept = (CheckBox)findViewById(R.id.cbAccept);
+        mCbAccept = (CheckBox)findViewById(R.id.cb_accept);
     }
 
     public void onClickNext(View view){
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         if(view != null) next  = view.getId();
         else return;
         switch (next){
-            case R.id.btnGo:
+            case R.id.btn_go:
                 if(mCbAccept.isChecked()){
                     setContentView(R.layout.question_one);
                 }else{
@@ -32,40 +32,40 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                 }
                 break;
-            case R.id.btnNext1:
+            case R.id.btn_next1:
                 setContentView(R.layout.question_two);
                 break;
-            case R.id.btnNext2:
+            case R.id.btn_next2:
                 setContentView(R.layout.question_three);
                 break;
-            case R.id.btnNext3:
+            case R.id.btn_next3:
                 setContentView(R.layout.question_four);
                 break;
-            case R.id.btnNext4:
+            case R.id.btn_next4:
                 setContentView(R.layout.question_five);
                 break;
-            case R.id.btnNext5:
+            case R.id.btn_next5:
                 setContentView(R.layout.question_six);
                 break;
-            case R.id.btnNext6:
+            case R.id.btn_next6:
                 setContentView(R.layout.question_seven);
                 break;
-            case R.id.btnNext7:
+            case R.id.btn_next7:
                 setContentView(R.layout.question_eight);
                 break;
-            case R.id.btnNext8:
+            case R.id.btn_next8:
                 setContentView(R.layout.question_nine);
                 break;
-            case R.id.btnNext9:
+            case R.id.btn_next9:
                 setContentView(R.layout.question_ten);
                 break;
-            case R.id.btnNext10:
+            case R.id.btn_next10:
                 setContentView(R.layout.question_eleven);
                 break;
-            case R.id.btnNext11:
+            case R.id.btn_next11:
                 setContentView(R.layout.question_twelve);
                 break;
-            case R.id.btnNext12:
+            case R.id.btn_next12:
                 setContentView(R.layout.finish_survey);
                 break;
             default:
