f19ae271525082f5c5a377d476d05942c52201d1
尘外孤标 <3334684495@qq.com>
2020-03-07 16:24:21 +0800

    next is ok

--- app/src/main/java/mg/studio/android/survey/MainActivity.java
+++ app/src/main/java/mg/studio/android/survey/MainActivity.java
@@ -9,208 +9,217 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.jetbrains.annotations.Nullable;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private CheckBox mCbAccept;
-    private String[] answers=null;
+    private String[] answers = null;
+    private JSONArray questions;
+    private Map<String, String> survey;
     static AppCompatActivity mainActivity;
+    private int qNum = 0;
+    private int qSeq = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome);
-        mCbAccept = (CheckBox)findViewById(R.id.cb_accept);
+        mCbAccept = (CheckBox) findViewById(R.id.cb_accept);
         mainActivity = this;
+        // initial question list
+        questions = GetQuestions();
+        if (questions == null) {
+            qNum = 0;
+        } else {
+            qNum = questions.length();
+        }
+        qSeq = 0;
     }
 
-    public void onClickNext(View view){
-        int next;
-        if(view != null) next  = view.getId();
-        else return;
-        switch (next){
-            case R.id.btn_go:{
-                if(mCbAccept.isChecked()){
-                    answers = new String[12];
-                    initialUserId();
-                    setContentView(R.layout.question_one);
-                }else{
-                    Toast.makeText(
-                            this, R.string.accept_requests,
-                            Toast.LENGTH_SHORT).show();
-                }
-                break;
+    // initial question list
+    @Nullable
+    private JSONArray GetQuestions() {
+        try {
+            // read sample.json
+            InputStreamReader inputReader = new InputStreamReader(
+                    getAssets().open("sample.json"));
+            BufferedReader buffReader = new BufferedReader(inputReader);
+            String line = "";
+            StringBuilder text = new StringBuilder();
+            while ((line = buffReader.readLine()) != null) {
+                text.append(line.trim());
             }
-            case R.id.btn_next1:{
-                answers[0] = getAnswer(R.id.rg_question1);
-                if (answers[0].length() == 0) break;
-                setContentView(R.layout.question_two);
-                break;
-            }
-            case R.id.btn_next2:{
-                answers[1] = getAnswer(R.id.rg_question2);
-                if(answers[1].length() == 0) break;
-                setContentView(R.layout.question_three);
-                break;
-            }
-            case R.id.btn_next3:{
-                answers[2] = getAnswer(R.id.rg_question3);
-                if(answers[2].length() == 0) break;
-                setContentView(R.layout.question_four);
-                break;
-            }
-            case R.id.btn_next4:{
-                answers[3]="Your answer is ";
-                int [] checkedIds = new int[7];
-                checkedIds[0] = R.id.cb_music;
-                checkedIds[1] = R.id.cb_photo;
-                checkedIds[2] = R.id.cb_game;
-                checkedIds[3] = R.id.cb_business;
-                checkedIds[4] = R.id.cb_gps;
-                checkedIds[5] = R.id.cb_data;
-                checkedIds[6] = R.id.cb_others;
-                for (int i=0;i<7;i++){
-                    CheckBox cb = (CheckBox) findViewById(checkedIds[i]);
-                    if(cb.isChecked()){
-                        answers[3] +=  cb.getText().toString();
-                        answers[3] += ",";
-                    }
-                }
-                if(answers[3].equals("Your answer is ")){
-                    Toast.makeText(this,
-                            R.string.empty_answer,
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                }
-                setContentView(R.layout.question_five);
-                break;
-            }
-            case R.id.btn_next5:{
-                answers[4]="Your answer is ";
-                int [] checkedIds = new int[7];
-                checkedIds[0] = R.id.cb_music;
-                checkedIds[1] = R.id.cb_photo;
-                checkedIds[2] = R.id.cb_game;
-                checkedIds[3] = R.id.cb_business;
-                checkedIds[4] = R.id.cb_gps;
-                checkedIds[5] = R.id.cb_data;
-                checkedIds[6] = R.id.cb_others;
-                for (int i=0;i<7;i++){
-                    CheckBox cb = (CheckBox) findViewById(checkedIds[i]);
-                    if(cb.isChecked()){
-                        answers[4] +=  cb.getText().toString();
-                        answers[4] += ",";
-                    }
-                }
-                if(answers[4].equals("Your answer is ")){
-                    Toast.makeText(this,
-                            R.string.empty_answer,
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                }
-                setContentView(R.layout.question_six);
-                break;
-            }
-            case R.id.btn_next6:{
-                EditText editText = (EditText) findViewById(R.id.edt_answer6);
-                answers[5] = "Your answer is ";
-                answers[5] += editText.getText().toString();
-                if (answers[5].length() == 0) {
-                    Toast.makeText(this,
-                            R.string.empty_answer,
-                            Toast.LENGTH_SHORT).show();
-                    break;
-                }
-                setContentView(R.layout.question_seven);
-                break;
-            }
-            case R.id.btn_next7:{
-                answers[6] = getAnswer(R.id.rg_question7);
-                if(answers[6].length() == 0) break;
-                setContentView(R.layout.question_eight);
-                break;
-            }
-            case R.id.btn_next8:{
-                answers[7] = getAnswer(R.id.rg_question8);
-                if(answers[7].length() == 0) break;
-                setContentView(R.layout.question_nine);
-                break;
-            }
-            case R.id.btn_next9:{
-                answers[8] = getAnswer(R.id.rg_question9);
-                if(answers[8].length() == 0) break;
-                setContentView(R.layout.question_ten);
-                break;
-            }
-            case R.id.btn_next10: {
-                answers[9] = getAnswer(R.id.rg_question10);
-                if (answers[9].length() == 0) break;
-                setContentView(R.layout.question_eleven);
-                break;
-            }
-            case R.id.btn_next11:{
-                answers[10] = getAnswer(R.id.rg_question11);
-                if(answers[10].length() == 0) break;
-                setContentView(R.layout.question_twelve);
-                break;
-            }
-            case R.id.btn_next12:{
-                answers[11] = getAnswer(R.id.rg_question12);
-                if(answers[11].length() == 0) break;
-                setContentView(R.layout.finish_survey);
-                break;
-            }
-            default:
-                    break;
+            inputReader.close();
+            // analyse text
+            JSONObject json = new JSONObject(text.toString());
+            JSONObject survey = json.getJSONObject("survey");
+            // return question json object list
+            return survey.getJSONArray("questions");
+        } catch (IOException ioe) {
+            return null;
+        } catch (JSONException je) {
+            return null;
         }
     }
 
-    public void Finish(View view) throws IOException {
-        Intent intent = new Intent(this, ReportActivity.class);
-        intent.putExtra("answer",answers);
-        startActivity(intent);
-    }
-
-    private void initialUserId(){
-        SharedPreferences sp = getSharedPreferences(
-                "user_id",MODE_PRIVATE);
-        if(sp.getInt("id",-1)<0){
-            SharedPreferences.Editor editor = sp.edit();
-            editor.putInt("id",0);
-            editor.commit();
+    public void setSingleView(JSONObject ques,
+                              int title_id) throws JSONException {
+        if (ques == null) return;
+        setContentView(R.layout.question_single);
+        ((TextView) findViewById(R.id.title)).setText(title_id);
+        ((TextView) findViewById(R.id.question)).setText(ques.getString("question"));
+        JSONArray jArray = ques.getJSONArray("options");
+        int size = jArray.length();
+        String[] optionText = new String[size];
+        RadioGroup rGroup = findViewById(R.id.options);
+        RadioGroup.LayoutParams lp = new RadioGroup.LayoutParams(
+                RadioGroup.LayoutParams.MATCH_PARENT,
+                RadioGroup.LayoutParams.WRAP_CONTENT);
+        for (int i = 0; i < size; i++) {
+            optionText[i] = ((JSONObject) jArray.get(i)).getString(String.valueOf(i + 1));
+            RadioButton option = new RadioButton(this);
+            option.setText(optionText[i]);
+            option.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                    getResources().getDimension(R.dimen.font_size_average));
+            option.setPadding(0,5,0,5);
+            rGroup.addView(option, lp);
         }
     }
 
-    private String getAnswer(int rgId){
-        RadioGroup rg = (RadioGroup) findViewById(rgId);
-        int checkedId = rg.getCheckedRadioButtonId();
-        if(checkedId == -1){
-            Toast.makeText(this,
-                    R.string.empty_answer,
+    public void setMultipleView(JSONObject ques,
+                                int title_id) throws JSONException {
+        if (ques == null) return;
+        setContentView(R.layout.question_multiple);
+        ((TextView) findViewById(R.id.title)).setText(title_id);
+        ((TextView) findViewById(R.id.question)).setText(ques.getString("question"));
+        JSONArray jArray = ques.getJSONArray("options");
+        int size = jArray.length();
+        String[] optionText = new String[size];
+        LinearLayout lLayout = findViewById(R.id.options);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        for (int i = 0; i < size; i++) {
+            optionText[i] = ((JSONObject) jArray.get(i)).getString(String.valueOf(i + 1));
+            CheckBox cb = new CheckBox(this);
+            cb.setText(optionText[i]);
+            cb.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                    getResources().getDimension(R.dimen.font_size_average));
+            cb.setPadding(0,5,0,5);
+            lLayout.addView(cb, lp);
+        }
+    }
+
+    public void setFillView(JSONObject ques,
+                            int title_id) throws JSONException {
+        setContentView(R.layout.question_fill);
+        ((TextView) findViewById(R.id.title)).setText(title_id);
+        ((TextView) findViewById(R.id.question)).setText(ques.getString("question"));
+    }
+
+    public void onClickGo(View view) {
+        if (mCbAccept.isChecked()) {
+            goNextPage();
+        } else {
+            Toast.makeText(this, R.string.accept_requests,
                     Toast.LENGTH_SHORT).show();
-            return "";
         }
-        RadioButton rb = (RadioButton) findViewById(checkedId);
-        return "Your answer is "+rb.getText().toString();
     }
+
+    public void onClickSingleNext(View view) {
+        RadioGroup rGroup = findViewById(R.id.options);
+        int checkedId = rGroup.getCheckedRadioButtonId();
+        if (checkedId > 0) {
+            String answer = ((RadioButton) findViewById(checkedId)).getText().toString();
+            Log.i("onClickSingleNext", answer);
+            goNextPage();
+        }else{
+            Toast.makeText(this, R.string.empty_answer,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onClickMultipleNext(View view) {
+        LinearLayout lLayout = findViewById(R.id.options);
+        int n = lLayout.getChildCount();
+        int x = 0;
+        for (int i = 0; i < n; i++) {
+            CheckBox cb = (CheckBox) lLayout.getChildAt(i);
+            if (cb.isChecked()) {
+                String answer = cb.getText().toString();
+                Log.i("onClickMultipleNext", answer);
+                x++;
+            }
+        }
+        if(x>0){
+            goNextPage();
+        }else{
+            Toast.makeText(this, R.string.empty_answer,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onClickFillNext(View view) {
+        EditText editText = findViewById(R.id.answer_text);
+        String answer = editText.getText().toString();
+        if(answer.length() != 0){
+
+            goNextPage();
+        }else{
+            Toast.makeText(this, R.string.empty_answer,
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void goNextPage() {
+        try {
+            if (qSeq < qNum) {
+                JSONObject question = ((JSONObject) questions.get(qSeq++));
+                String type = question.getString("type");
+                if (type.equals("single")) {
+                    setSingleView(question, R.string.single);
+                } else if (type.equals("multiple")) {
+                    setMultipleView(question, R.string.multiple);
+                } else if (type.equals("fill")) {
+                    setFillView(question, R.string.fill);
+                }
+            } else {
+                setContentView(R.layout.finish_survey);
+            }
+        } catch (JSONException je) {
+            je.printStackTrace();
+        }
+    }
+
 }
