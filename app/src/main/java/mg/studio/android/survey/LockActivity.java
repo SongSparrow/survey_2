69db08b69dff0204e24f5882923a99b9ed7774cf
尘外孤标 <3334684495@qq.com>
2020-03-13 22:05:13 +0800

    add scan and lock ui

---
+++ app/src/main/java/mg/studio/android/survey/LockActivity.java
@@ -0,0 +1,21 @@
+package mg.studio.android.survey;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlock);
+    }
+
+    public void onClickSetPswd(View view) {
+    }
+
+    public void onClickUnlock(View view) {
+    }
+}
